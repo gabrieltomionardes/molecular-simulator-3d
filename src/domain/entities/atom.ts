db22f54a23b08998f627fd2d7ff5af36bb03ef66
Gabriel Tomio Nardes <gabrieltomio@protonmail.com>
2020-12-16 00:08:39 -0300

    feat: create initial atom entity

---
+++ src/domain/entities/atom.ts
@@ -0,0 +1,20 @@
+import { Result } from "../../shared/result"
+
+export class Atom {
+  private readonly _symbol: string
+  private constructor (symbol: string) {
+    this._symbol = symbol
+  }
+
+  public static create (symbol: string): Result<Atom> {
+    if (symbol.length === 0) {
+      return Result.fail<Atom>(new Error('Simbolo não pode ser vázio'))
+    }
+
+    return Result.ok<Atom>(new Atom(symbol))
+  }
+
+  public get symbol () {
+    return this._symbol
+  }
+}
