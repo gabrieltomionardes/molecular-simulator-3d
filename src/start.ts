86cc21c6a6af33e0e1df8d7079bd179ca89afc92
Gabriel Tomio Nardes <gabrieltomio@protonmail.com>
2020-12-19 02:05:44 -0300

    feat: remove start file

--- src/start.ts
+++
@@ -1,9 +0,0 @@
-import { Atom } from './domain/entities/atom'
-
-const maybeCabon = Atom.create('')
-
-if (maybeCabon.isFailure) {
-  console.log('Erro: ', maybeCabon?.error?.message)
-} else {
-  console.log('Carbono: ', maybeCabon?.value?.symbol)
-}
