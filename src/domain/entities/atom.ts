58b62ec00ea0bb80e616ad687e6b783ad7e288c5
Gabriel Tomio Nardes <gabrieltomio@protonmail.com>
2020-12-19 03:25:00 -0300

    feat: add valence attribute in atom

--- src/domain/entities/atom.ts
+++ src/domain/entities/atom.ts
@@ -1,20 +1,38 @@
 import { Result } from '../../shared/result'
 
+export type AtomProps = {
+  symbol: string
+  valence: number
+}
 export class Atom {
   private readonly _symbol: string
-  private constructor (symbol: string) {
-    this._symbol = symbol
+  private readonly _valence: number
+  private _chemicalBond: number = 0
+
+  private constructor (props: AtomProps) {
+    this._symbol = props.symbol
+    this._valence = props.valence
   }
 
-  public static create (symbol: string): Result<Atom> {
+  public static create (props: AtomProps): Result<Atom> {
+    const { symbol, valence } = props
+
     if (symbol.length === 0) {
-      return Result.fail<Atom>(new Error('Simbolo não pode ser vázio'))
+      return Result.fail<Atom>(new Error('The symbol can not be empty'))
     }
 
-    return Result.ok<Atom>(new Atom(symbol))
+    if (valence < 0) {
+      return Result.fail<Atom>(new Error('The valence can not be negative'))
+    }
+
+    return Result.ok<Atom>(new Atom(props))
   }
 
-  public get symbol () {
-    return this._symbol
+  public bond (quantity = 1): void {
+    this._chemicalBond += quantity
+  }
+
+  public get isStable (): boolean {
+    return this._valence === this._chemicalBond
   }
 }
